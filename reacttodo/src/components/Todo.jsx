194a43536d90a55ed904bdde7858d99f12740b6f
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-09-07 11:30:00 +0530

    trying file upload

--- reacttodo/src/components/Todo.jsx
+++ reacttodo/src/components/Todo.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import "./Todo.css"
 export const Todo =()=>{
+    const [image,setImage]=useState("");
     const [todos,setTodos]=useState("");
     const [datas,setData]=useState([]);
     const [linethrough,setLinethrough]=useState(false);
@@ -14,17 +15,33 @@ export const Todo =()=>{
         });
    
     },[]);
+    const handlesubmit=(e)=>{
+        console.log("Submitted")
+        e.preventDefault();
+        const fromData=new FormData();
+        fromData.append("image",image)
+
+        console.log(fromData,image)
+        axios.post("http://localhost:8008/todos",fromData).then((res)=>{
+            console.log(res)
+        })
+        
+    }
 
     useEffect(()=>{
-
+    
     },[])
 
     const handlestyle=(id)=>{
         console.log(id)
     }
-
+   
     return(<>
         <div className="main">
+            {/* <form action="http://localhost:8008/todos" method="post" encType="multipart/form-data" > */}
+             <input type="file"    onChange={(e)=>{console.log(e.target.files[0]),setImage(e.target.files[0])}} />
+             {/* <button onClick={handlesubmit}> Submit</button> */}
+            {/* </form> */}
             <input onChange={(e)=>{
                 setTodos(e.target.value)
             }} type="text"  placeholder={` +  Add a To-do.. ` } value={todos}/>
@@ -35,6 +52,7 @@ export const Todo =()=>{
                 const payload={
                     title:todos,
                     state:false,
+                    pic:image
                 }
                 console.log(datas);
                 axios.post("http://localhost:8008/todos",payload);
@@ -59,7 +77,7 @@ export const Todo =()=>{
   
 
 
-         <div style={{color:'red',textDecorationLine: 'line-through', textDecorationStyle: 'solid'}}>color</div>
+         {/* <div style={{color:'red',textDecorationLine: 'line-through', textDecorationStyle: 'solid'}}>color</div> */}
         </>
     )
 }
